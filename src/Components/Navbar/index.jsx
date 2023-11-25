f25b730b4664e3f48c97741fb43f11e390b0edcb
AndresUg98 <oscaran2109@gmail.com>
2023-11-24 19:42:42 -0600

    continue wokting on the navbar

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { NavLink, useNavigate } from "react-router-dom";
+import { MdCatchingPokemon } from "react-icons/md";
 import "./Navbar.scss";
 function Navbar() {
   const navigate = useNavigate();
   return (
     <nav className="Navbar">
       <ul className="Navbar-elements">
-        <li>
-          <NavLink to="/PokeTeam">Team</NavLink>
+        <li className="Navbar-team">
+          <NavLink to="/PokeTeam">
+            <MdCatchingPokemon />
+          </NavLink>
         </li>
-        <li>
+        <li className="Navbar-home">
           <NavLink to="/">Home</NavLink>
         </li>
-        <li>
+        <li className="Navbar-back">
           <NavLink onClick={() => navigate(-1)}>Back</NavLink>
         </li>
       </ul>
