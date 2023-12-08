dfaca2c6a482aae5e16977630bfc0ccc0c98a4c8
AndresUg98 <oscaran2109@gmail.com>
2023-12-08 12:43:10 -0600

    making adjustments to the infinite scroll

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,103 +1,15 @@
 import React, { useState, useEffect, useContext } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { usePokemons } from "../../hooks/usePokemons";
 import { PokemonContext } from "../../Context";
 import { SearchBar } from "../../Components/SearchBar";
 import { PokeCard } from "../../Components/PokeCard";
 import "./Home.scss";
 
 function Home() {
-  //Getting all the pokemons and putting then into the pokemons state
-  const [pokemons, setPokemons] = useState(null);
-  //  consuming the pokemon api
-  const pokeApi = "https://pokeapi.co/api/v2/pokemon?limit=1008";
+  const { pokemons, loadPokemons, loadMorePokemons } = usePokemons();
 
-  //Visible pokemons will help us to store the pokemons that we want to be seen, so we don't overload the page
-  const [visiblePokemon, setVisiblePokemon] = useState([]);
-
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const getPokemons = async () => {
-      try {
-        //Recuperar la lista de los pokemones
-        const response = await fetch(pokeApi);
-        const pokemonArray = await response.json();
-
-        const { results } = pokemonArray;
-
-        const NewPokemons = results.map(async (pokemon) => {
-          const response = await fetch(pokemon.url);
-          const poke = await response.json();
-
-          //Obtaining the types of the poekemon
-          let types = poke.types.map((type) => type.type.name);
-          //Obtaining the abilities of the poekemon
-          let abilities = poke.abilities.map((ability) => ability.ability.name);
-          //Obtaining the stats of the poekemon
-          let stats = poke.stats.map((stat) => stat.base_stat);
-
-          //obtaining the description of each pokemon
-          let res;
-          res = await fetch(poke["species"]["url"]);
-          let pokemonDescription = await res.json();
-          pokemonDescription =
-            pokemonDescription["flavor_text_entries"][0]["flavor_text"];
-
-          //console.log(pokemonDescription);
-
-          return {
-            id: poke.id,
-            name: poke.name,
-            entry: pokemonDescription,
-            img: poke.sprites.other["official-artwork"].front_default,
-            weight: poke.weight,
-            height: poke.height,
-            order: poke.order,
-            types: types,
-            ability: abilities,
-            stats: stats,
-          };
-        }, context.setLoading(false));
-
-        setPokemons(await Promise.all(NewPokemons));
-      } catch (error) {
-        console.error("Error fetching Pokémon data:", error);
-      }
-    };
-
-    getPokemons();
-  }, []);
-
-  useEffect(() => {
-    //loading de data progressively at the moment of doing scroll, so we can implement the infinite scroll
-    const handleScroll = () => {
-      if (
-        window.innerHeight + document.documentElement.scrollTop ===
-          document.documentElement.offsetHeight &&
-        !loading
-      ) {
-        //the user has reach the end of the page, loading more pokemon
-        loadMorePokemon();
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [loading]);
-
-  const loadMorePokemon = () => {
-    setLoading(true);
-
-    // it make a pause so the data can load
-    setTimeout(() => {
-      const nextPage = visiblePokemon.length / 151 + 1;
-      const nextPokemon = pokemons.slice(nextPage * 151, (nextPage + 1) * 151);
-      setVisiblePokemon((prevPokemon) => [...prevPokemon, ...nextPokemon]);
-      setLoading(false);
-    }, 1000); // Puedes ajustar el tiempo de espera según tus necesidades
-  };
+  //console.log(pokemons);
 
   const context = useContext(PokemonContext);
 
@@ -109,13 +21,20 @@ function Home() {
         <SearchBar />
       </section>
 
-      <section className="Home-content">
-        {loading && <p>Estamos cargando</p>}
-        {context.error && <p>Error</p>}
+      <InfiniteScroll
+        dataLength={pokemons.length}
+        next={loadPokemons}
+        hasMore={loadMorePokemons}
+        loader={<h4>Cargando</h4>}
+        endMessage={<h4>No hay mas</h4>}
+        className="Home-content"
+      >
+        {/* {context.loading && <p>Estamos cargando</p>}
+        {context.error && <p>Error</p>} */}
         {pokemons?.map((pokemon) => {
           return <PokeCard key={pokemon.name} data={pokemon} />;
         })}
-      </section>
+      </InfiniteScroll>
     </div>
   );
 }
---
+++ src/hooks/usePokemons.jsx
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from "react";
+const URL_DEFAULT = "https://pokeapi.co/api/v2/pokemon?limit=63&offset=0";
+
+function usePokemons() {
+  //  consuming the pokemon api
+
+  //Getting all the pokemons and putting then into the pokemons state
+  const [pokemons, setPokemons] = useState([]);
+
+  const [siguienteUrl, setSiguienteUrl] = useState("");
+
+  const [loadMorePokemons, setLoadMorePokemons] = useState(true);
+
+  const getPokemons = async (url = URL_DEFAULT) => {
+    //Recuperar la lista de los pokemones
+    const response = await fetch(url);
+    const pokemonArray = await response.json();
+    // we sustract the next and results attributes from the response, next redirect you to the next 20 results
+    const { next, results } = pokemonArray;
+
+    const NewPokemons = await Promise.all(
+      results.map(async (pokemon) => {
+        const response = await fetch(pokemon.url);
+        const poke = await response.json();
+
+        //Obtaining the types of the poekemon
+        let types = poke.types.map((type) => type.type.name);
+        //Obtaining the abilities of the poekemon
+        let abilities = poke.abilities.map((ability) => ability.ability.name);
+        //Obtaining the stats of the poekemon
+        let stats = poke.stats.map((stat) => stat.base_stat);
+
+        //obtaining the description of each pokemon
+        let res;
+        res = await fetch(poke["species"]["url"]);
+        let pokemonDescription = await res.json();
+        pokemonDescription =
+          pokemonDescription["flavor_text_entries"][0]["flavor_text"];
+
+        //console.log(pokemonDescription);
+
+        return {
+          id: poke.id,
+          name: poke.name,
+          entry: pokemonDescription,
+          img: poke.sprites.other["official-artwork"].front_default,
+          weight: poke.weight,
+          height: poke.height,
+          order: poke.id,
+          types: types,
+          ability: abilities,
+          stats: stats,
+        };
+      })
+    );
+    return { next, NewPokemons };
+  };
+
+  const obtenerPokemones = async () => {
+    const { next, NewPokemons } = await getPokemons(); //the function getPokemos returns de "next" variable and de array of pokemons "NewPokemons"
+    setPokemons(NewPokemons);
+    setSiguienteUrl(next);
+  };
+
+  const loadPokemons = async () => {
+    const { next, NewPokemons } = await getPokemons(siguienteUrl);
+    setPokemons((prev) => [...prev, ...NewPokemons]);
+    next == null && setLoadMorePokemons(false);
+    setSiguienteUrl(next);
+  };
+
+  useEffect(() => {
+    obtenerPokemones();
+  }, []);
+
+  return {
+    pokemons,
+    loadPokemons,
+    loadMorePokemons,
+  };
+}
+
+export { usePokemons };
