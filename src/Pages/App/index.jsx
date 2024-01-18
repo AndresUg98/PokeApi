80a8cc02bd0fd46fd39c4de4bd00b02fb19e580b
AndresUg98 <oscaran2109@gmail.com>
2024-01-18 11:53:15 -0600

    :fire: removing unnecessary files

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,15 +1,11 @@
 import { useRoutes, BrowserRouter } from "react-router-dom";
 import { Home } from "../Home";
 import { PokemonProvider } from "../../Context";
-import { NotFound } from "../NotFound";
 import { Navbar } from "../../Components/Navbar";
 import "./App.css";
 
 const AppRoutes = () => {
-  let routes = useRoutes([
-    { path: "/", element: <Home /> },
-    { path: "/*", element: <NotFound /> },
-  ]);
+  let routes = useRoutes([{ path: "/", element: <Home /> }]);
   return routes;
 };
 
--- src/Pages/NotFound/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function NotFound() {
-  return <div>NotFound</div>;
-}
-
-export { NotFound };
